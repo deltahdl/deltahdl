a03d8f522ed8ec9ecbd510da68c194073fec6b91
Jose Drowne <jdrowne@10ulabs.com>
2026-03-21 09:42:31 -0400

    Implement §6.16.13
    - Modified `test/src/unit/test_simulator_clause_06_16_13.cpp` because §6.16.13 edge cases were untested: added `OcttoaZero` and `OcttoaOverwritesExisting`
    
    Closes #676

--- test/src/unit/test_simulator_clause_06_16_13.cpp
+++ test/src/unit/test_simulator_clause_06_16_13.cpp
@@ -14,4 +14,20 @@ TEST(StringMethods, Octtoa) {
   EXPECT_EQ(VecToString(var->value), "10");
 }
 
+TEST(StringMethods, OcttoaZero) {
+  StringFixture f;
+  auto* var = f.CreateStringVar("s", "");
+  auto* call = f.MakeMethodCall("s", "octtoa", {f.MakeIntLiteral(0)});
+  EvalExpr(call, f.ctx, f.arena);
+  EXPECT_EQ(VecToString(var->value), "0");
+}
+
+TEST(StringMethods, OcttoaOverwritesExisting) {
+  StringFixture f;
+  auto* var = f.CreateStringVar("s", "old");
+  auto* call = f.MakeMethodCall("s", "octtoa", {f.MakeIntLiteral(64)});
+  EvalExpr(call, f.ctx, f.arena);
+  EXPECT_EQ(VecToString(var->value), "100");
+}
+
 }  // namespace
